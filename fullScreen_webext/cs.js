fc4f0a1249de01a43b5464da1038ad7280d092a2
Valeriy Manzhos <valeronishe2002@gmail.com>
2020-11-17 13:32:33 +0300

    Replace `Ctrl+Shift+F` with `Ctrl+Shift+V`

--- fullScreen_webext/cs.js
+++ fullScreen_webext/cs.js
@@ -1,7 +1,7 @@
 const allowFullScreen = event => {
     event.preventDefault();
     event = event || window.event;
-    if (event.ctrlKey && event.keyCode == 70 && event.shiftKey) {
+    if (event.ctrlKey && event.keyCode == 86 && event.shiftKey) {
         if(document.documentElement.webkitRequestFullScreen) document.documentElement.webkitRequestFullScreen();
         else if(document.documentElement.mozRequestFullScreen) document.documentElement.mozRequestFullScreen();
     }
